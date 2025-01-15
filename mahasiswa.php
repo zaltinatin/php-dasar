4565902832352a3496d8e09aac6ba48b940237aa
zaltinatin <zaltin132@gmail.com>
2025-01-15 21:46:13 +0700

    Add files via upload

---
+++ mahasiswa.php
@@ -0,0 +1,56 @@
+<?php
+
+    $mysqli = new mysqli('localhost', 'root', '', 'mahasiswa');
+    $result = $mysqli->query("SELECT siswa.NIM, siswa.NAMA, program_studi.Prodi
+    FROM siswa INNER JOIN program_studi ON siswa.ID = program_studi.Nomor");
+
+   $siswa = [];
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($siswa, $row);
+    }
+
+    $Nomor = 1
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta Prodi="viewport" content="wIDth=device-wIDth, initial-scale=1.0">
+    <title>siswa</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="aNomornymous">
+
+    <style>
+    table {
+        font-family: 'Times New Roman', Times, serif;
+    }
+    </style>
+    
+</head>
+<body>
+    <div class="container"> 
+    <h1 class="text-center">Data siswa KA 2021</h1>   
+    <table class="table table-bordered table-hover">
+        <tr>
+            <th class="text-center">Nomor</th>
+            <th class="text-center">NAMA</th>
+            <th class="text-center">NIM</th>
+            <th class="text-center">Program Studi</th>
+            <th class="text-center">Edit</th>
+        </tr>
+        <?php foreach ($siswa as $row ) { ?>
+            <tr>
+                <td><?= $Nomor++; ?></td>
+                <td><?= $row['NIM']; ?></td>
+                <td><?= $row['NAMA']; ?></td>
+                <td><?= $row['Prodi']; ?></td>   
+        </tr>
+        <?php } ?>
+    </table>  
+    </div>  
+</body>
+</html>
+
---
+++ mahasiswa.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jan 2025 pada 13.58
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `program_studi`
+--
+
+CREATE TABLE `program_studi` (
+  `Nomor` int(20) NOT NULL,
+  `Prodi` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `NIM` varchar(10) NOT NULL,
+  `NAMA` varchar(50) DEFAULT NULL,
+  `ID` int(7) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
